6930a99a54db0deca97d564bdad28af0c971c0a2
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-19 17:26:35 -0300

    Atualização das rotas

--- routes/api.php
+++ routes/api.php
@@ -11,6 +11,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('dashboard', [DashboardController::class, 'index']);
 
+    Route::get('exercises', [ExerciseController::class, 'index']);
     Route::post('exercises', [ExerciseController::class, 'store']);
 
 });
