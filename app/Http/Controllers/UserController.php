d19084f3ee47a8f2d5ee83cbbb10e204a37ce6a3
EduardoPSRodrigues <eduardo_rodrigues10@estudante.sesisenai.org.br>
2023-12-27 17:03:02 -0300

    Atualizando para enviar e-mail de cadastro para o usuário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\SendWelcomeEmailToUser;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 use Symfony\Component\HttpFoundation\Response;
 
 class UserController extends Controller
 {
-    public function store(Request $request) {
-       try {
-        $data = $request->all();
+    public function store(Request $request)
+    {
+        try {
+            $data = $request->all();
 
-        $request->validate([
-            'name' => 'string|max:255|required',
-            'email' => 'string|email|max:255|required|unique:users',
-            'date_birth' => 'date_format:Y-m-d|required',
-            'cpf' => 'string|max:14|required|unique:users',
-            'password'=> 'string|min:8|max:32|required',
-            'plan_id'=> 'integer|required'
-        ]);
+            $request->validate([
+                'name' => 'string|max:255|required',
+                'email' => 'string|email|max:255|required|unique:users',
+                'date_birth' => 'date_format:Y-m-d|required',
+                'cpf' => 'string|max:14|required|unique:users',
+                'password' => 'string|min:8|max:32|required',
+                'plan_id' => 'integer|required'
+            ]);
 
-        $user = User::create($data);
+            $user = User::create($data);
 
-        return $user;
+            Mail::to($data['email'], $data['name'])
+                ->send(new SendWelcomeEmailToUser($user));
 
-       } catch (\Exception $exception) {
-        return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
-       }
+            return $user;
+        } catch (\Exception $exception) {
+            return $this->error($exception->getMessage(), Response::HTTP_BAD_REQUEST);
+        }
     }
-
 }
